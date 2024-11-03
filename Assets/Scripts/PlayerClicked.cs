c076ab35b7e555ed88c5667306d6b6ed47651c93
Alejandro Smith <smithalejandro96@gmail.com>
2024-11-03 19:27:06 -0300

    client's player affected in all clients

--- Assets/Scripts/PlayerClicked.cs
+++ Assets/Scripts/PlayerClicked.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerClicked : NetworkBehaviour {
     private SpriteRenderer _spriteRenderer;
+    [SerializeField] private string selectedLetter = "F";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,21 +15,45 @@ public class PlayerClicked : NetworkBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("F key was pressed!");
+            selectedLetter = "F";
+        }
+        else if (Input.GetKey(KeyCode.Q)) // Replace KeyCode.Q with the key you want to detect
+        {
+            Debug.Log("Q key is being held down!");
+            selectedLetter = "Q";
+        }
     }
 
     [Rpc(SendTo.ClientsAndHost)]
-    private void ChangeSpriteColorRpc() {
+    private void ChangeSpriteColorGreenRpc() {
         _spriteRenderer.color = Color.green;
     }
     
+    [Rpc(SendTo.ClientsAndHost)]
+    private void ChangeSpriteColorRedRpc() {
+        _spriteRenderer.color = Color.red;
+    }
+    [Rpc(SendTo.ClientsAndHost)]
+    private void ChangeSpriteColorDefaultRpc() {
+        _spriteRenderer.color = Color.white;
+    }
+    
     [Rpc(SendTo.Server)]
-    void HandlePlayerClickedRpc(RpcParams rpcParams = default) {
+    void HandlePlayerClickedRpc(string selectedLetter, RpcParams rpcParams = default) {
         var clientId = rpcParams.Receive.SenderClientId;
-        var targetObjectId = NetworkObjectId;
-        Debug.Log($"Client {clientId} clicked on object {targetObjectId}");
-        //ChangeSpriteColorRpc(targetObjectId);
-        ChangeSpriteColorRpc();
+        var objectId = NetworkObjectId;
+        Debug.Log($"Client {clientId} clicked on object {objectId}");
+        
+        // if server says its valid action
+        ChangeSpriteColorGreenRpc();
+        
+        if (selectedLetter == "F")
+            NetworkManager.ConnectedClients[clientId].PlayerObject.GetComponent<PlayerClicked>().ChangeSpriteColorRedRpc();
+        if (selectedLetter == "Q")
+            NetworkManager.ConnectedClients[clientId].PlayerObject.GetComponent<PlayerClicked>().ChangeSpriteColorDefaultRpc();
     }
 
     private void OnMouseDown() {
@@ -39,6 +64,6 @@ public class PlayerClicked : NetworkBehaviour {
 
         // Send both player IDs to the server for processing
         //HandlePlayerClickedRpc(clientId, objectId);
-        HandlePlayerClickedRpc();
+        HandlePlayerClickedRpc(selectedLetter);
     }
 }

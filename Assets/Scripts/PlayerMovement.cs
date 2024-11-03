ea59104a53f69b5b71df3ef4f865593a7f090f7c
Alejandro Smith <smithalejandro96@gmail.com>
2024-11-02 23:59:26 -0300

    player movement with network

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,12 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : NetworkBehaviour {
     
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D _rb;
-    private Vector2 _movement;
+    private readonly NetworkVariable<Vector2> _movement = new NetworkVariable<Vector2>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         _rb = GetComponent<Rigidbody2D>();
@@ -13,10 +14,18 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        _rb.linearVelocity = _movement * moveSpeed;
+        _rb.linearVelocity = _movement.Value * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context) {
-        _movement = context.ReadValue<Vector2>();
+        if (IsOwner) {
+            var direction = context.ReadValue<Vector2>();
+            MoveRpc(direction);
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void MoveRpc(Vector2 direction, RpcParams rpcParams = default) {
+        _movement.Value = direction * moveSpeed;
     }
 }

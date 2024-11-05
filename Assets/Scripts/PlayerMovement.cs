1323deb85e1b3d28439b2eb36212c5ca3794a1d0
Alejandro Smith <smithalejandro96@gmail.com>
2024-11-05 18:10:13 -0300

    client authoritative movement, it sucks but it's the best i was able to do

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,38 +6,34 @@ public class PlayerMovement : NetworkBehaviour {
     
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D _rb;
-    private readonly NetworkVariable<Vector2> _movement = new();
+    private Vector2 _movement = new();
     
     public override void OnNetworkSpawn() {
         base.OnNetworkSpawn();
         if (IsOwner) {
             GetComponent<PlayerInput>().enabled = true;
+            _rb = GetComponent<Rigidbody2D>();
         }
-    }
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        _rb.linearVelocity = _movement.Value * moveSpeed;
+        if (!IsOwner) return;
+        _rb.linearVelocity = _movement * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context) {
         if (!IsOwner) return;
         var direction = context.ReadValue<Vector2>();
-        MoveRpc(direction);
+        _movement = direction * moveSpeed;
     }
 
-    [Rpc(SendTo.Server)]
+    /*[Rpc(SendTo.Server)]
     private void MoveRpc(Vector2 direction, RpcParams rpcParams = default) {
         // server validates movement
         if (rpcParams.Receive.SenderClientId != OwnerClientId) {
             Debug.LogWarning($"Client {rpcParams.Receive.SenderClientId} attempted to control a player ({OwnerClientId}) they do not own.");
             return;
         }
-        _movement.Value = direction * moveSpeed;
-    }
+    }*/
 }
